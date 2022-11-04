cdc22c89efaad480e525b926d45460e8a9aaf78a
Pinush <gdergan@hotmail.com>
2022-11-04 01:52:09 -0500

    SSL agregado

--- serverai.js
+++ serverai.js
@@ -1,8 +1,20 @@
+const https = require("https");
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send("Hola mundo!"))
 
-app.listen(3000);
-console.log('Servidor en puerto 3000');
+
+var opciones = {
+    clave: fs.readFileSync('/cert/openeuralai.key'),
+    certificado: fs.readFileSync('/cert/1243627926.crt'),
+    ca: fs.readFileSync ('/cert/1243627926.ca-bundle')
+};
+
+https.createServer(opciones, app)
+    .listen(3000, ()=>{
+        console.log('server is runing at port 4000')
+    });
+
+app.get('/', (req, res) => res.send("Hola mundo!"))
